84da00aba96c63024c5389b252e303a7b0ad17db
carpentryplus25 <williamt1984@yahoo.com>
2018-11-20 20:56:34 -0500

    Initial Commit

---
+++ JWELLCFramer/JWELLCFramer.h
@@ -0,0 +1,19 @@
+//
+//  JWELLCFramer.h
+//  JWELLCFramer
+//
+//  Created by William Thompson on 11/20/18.
+//  Copyright © 2018 J.W.Enterprises LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JWELLCFramer.
+FOUNDATION_EXPORT double JWELLCFramerVersionNumber;
+
+//! Project version string for JWELLCFramer.
+FOUNDATION_EXPORT const unsigned char JWELLCFramerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JWELLCFramer/PublicHeader.h>
+
+
